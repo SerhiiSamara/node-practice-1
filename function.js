4b96db16f3c1be0d1ea3124f90d92466a92f3640
SerhiiSamara <ndaair@gmail.com>
2023-02-12 17:30:07 +0200

    1

---
+++ function.js
@@ -0,0 +1,17 @@
+const sum = (arg1, arg2) => {
+	return arg1 + arg2;
+}
+
+const minus = (arg1, arg2) => {
+	return arg1 - arg2;
+}
+
+const mult = (arg1, arg2) => {
+	return arg1 * arg2;
+}
+
+const devide = (arg1, arg2) => {
+	return arg1 / arg2;
+}
+
+module.exports = { sum, minus, mult, devide };
